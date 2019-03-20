99b9ce52557a97a4f097d44129b1e9c6c9bde59a
Andrew Williams <a.d.c.williams@bristol.ac.uk>
2019-03-20 08:14:03 +0000

    Reduce the default size of the array to fit in available memory.
    The kubernetes pods are only allocated 1GB each and the 90000x4000
    array wouldn't fit after other tasks are running. Also changed the
    exercise so that it better demonstrates inefficient use of
    non-contiguous memory.

--- src/arrays/example1/efficient.f90
+++ src/arrays/example1/efficient.f90
@@ -6,7 +6,7 @@ Program array_loop
   Integer, parameter  :: sp = REAL32
 
   ! Declare a two dimentsional array
-  Integer             :: arrName(90000, 4000)
+  Integer             :: arrName(50000, 4000)
 
   ! Declare variables to hold start and end time
   Real(kind=sp)       :: start_time, end_time
@@ -19,7 +19,7 @@ Program array_loop
 
   ! Populate the array
   do y = 1, 4000
-    do x = 1, 90000
+    do x = 1, 50000
       arrName(x, y) = x + y
     end do
   end do
--- src/arrays/example1/inefficient.f90
+++ src/arrays/example1/inefficient.f90
@@ -6,7 +6,7 @@ Program array_inefficient
   Integer, parameter  :: sp = REAL32
 
   ! Declare a two dimentsional array
-  Integer             :: arrName(90000, 4000)
+  Integer             :: arrName(50000, 4000)
 
   ! Declare variables to hold start and end time
   Real(kind=sp)       :: start_time, end_time
@@ -18,7 +18,7 @@ Program array_inefficient
   call cpu_time(start_time)
 
   ! Populate the array
-  do x = 1, 90000
+  do x = 1, 50000
     do y = 1, 4000
       arrName(x, y) = x + y
     end do
